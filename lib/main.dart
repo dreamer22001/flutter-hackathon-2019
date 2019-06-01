09e1ae969581010bcd90a774cf32cb018e50e80a
Rafael Bastiansch <rafaelbastiansch@gmail.com>
2019-06-01 12:41:11 -0300

    remove trahs of main

--- lib/main.dart
+++ lib/main.dart
@@ -39,46 +39,8 @@ class MyStatelessWidget extends StatelessWidget {
             },
           ),
         ],
-        // child: TextField(
-        //   decoration: InputDecoration(
-        //     border: InputBorder.none,
-        //     hintText: 'Please enter a search term'
-        //   ),
-        // ),
       ),
       body: Search(),
     );
   }
 }
-
-
-// // Define a Custom Form Widget
-// class MyCustomForm extends StatefulWidget {
-//   @override
-//   MyCustomFormState createState() {
-//     return MyCustomFormState();
-//   }
-// }
-
-// // Define a corresponding State class. This class will hold the data related to
-// // the form.
-// class MyCustomFormState extends State<MyCustomForm> {
-//   // Create a global key that will uniquely identify the Form widget and allow
-//   // us to validate the form
-//   //
-//   // Note: This is a `GlobalKey<FormState>`, not a GlobalKey<MyCustomFormState>!
-//   final _formKey = GlobalKey<FormState>();
-
-//   @override
-//   Widget build(BuildContext context) {
-//     // Build a Form widget using the _formKey we created above
-//     return Form(
-//       key: _formKey,
-//       child: TextFormField(
-//         decoration: InputDecoration(
-//           labelText: 'Enter your username'
-//         ),
-//       ),
-//     );
-//   }
-// }
