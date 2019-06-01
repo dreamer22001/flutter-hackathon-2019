eec689f4b4204bfc3e839402392b8989d945838d
Rafael Bastiansch <rafaelbastiansch@gmail.com>
2019-06-01 12:38:41 -0300

    add search component

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@
 // This sample shows adding an action to an [AppBar] that opens a shopping cart.
 
 import 'package:flutter/material.dart';
+import 'search.dart';
 
 void main() => runApp(MyApp());
 
@@ -30,7 +31,7 @@ class MyStatelessWidget extends StatelessWidget {
         title: Text('Ready, Set, Shop!'),
         actions: <Widget>[
           IconButton(
-            icon: Icon(Icons.shopping_cart),
+            icon: Icon(Icons.search),
             tooltip: 'Open shopping cart',
             onPressed: () {
               // Implement navigation to shopping cart page here...
@@ -38,7 +39,46 @@ class MyStatelessWidget extends StatelessWidget {
             },
           ),
         ],
+        // child: TextField(
+        //   decoration: InputDecoration(
+        //     border: InputBorder.none,
+        //     hintText: 'Please enter a search term'
+        //   ),
+        // ),
       ),
+      body: Search(),
     );
   }
 }
+
+
+// // Define a Custom Form Widget
+// class MyCustomForm extends StatefulWidget {
+//   @override
+//   MyCustomFormState createState() {
+//     return MyCustomFormState();
+//   }
+// }
+
+// // Define a corresponding State class. This class will hold the data related to
+// // the form.
+// class MyCustomFormState extends State<MyCustomForm> {
+//   // Create a global key that will uniquely identify the Form widget and allow
+//   // us to validate the form
+//   //
+//   // Note: This is a `GlobalKey<FormState>`, not a GlobalKey<MyCustomFormState>!
+//   final _formKey = GlobalKey<FormState>();
+
+//   @override
+//   Widget build(BuildContext context) {
+//     // Build a Form widget using the _formKey we created above
+//     return Form(
+//       key: _formKey,
+//       child: TextFormField(
+//         decoration: InputDecoration(
+//           labelText: 'Enter your username'
+//         ),
+//       ),
+//     );
+//   }
+// }
---
+++ lib/search.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+
+// Define a Custom Form Widget
+class Search extends StatefulWidget {
+  @override
+  MyCustomFormState createState() {
+    return MyCustomFormState();
+  }
+}
+
+// Define a corresponding State class. This class will hold the data related to
+// the form.
+class MyCustomFormState extends State<Search> {
+  // Create a global key that will uniquely identify the Form widget and allow
+  // us to validate the form
+  //
+  // Note: This is a `GlobalKey<FormState>`, not a GlobalKey<MyCustomFormState>!
+  final _formKey = GlobalKey<FormState>();
+  final _searchResult = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    // Build a Form widget using the _formKey we created above
+    return Form(
+      key: _formKey,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          TextFormField(
+            decoration: InputDecoration(
+              labelText: 'Enter your username',
+              contentPadding: const EdgeInsets.all(16.0),
+            ),
+            controller: _searchResult,
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 16.0),
+            child: RaisedButton(
+              onPressed: () {
+                // Validate will return true if the form is valid, or false if
+                // the form is invalid.
+                if (_formKey.currentState.validate()) {
+                  // If the form is valid, we want to show a Snackbar
+                  Scaffold.of(context)
+                      .showSnackBar(SnackBar(content: Text('Processing Data')));
+                  _performLogin();
+                }
+              },
+              child: Text('Submit'),
+            ),
+          ),
+        ],
+      )
+    );
+  }
+
+  void _performLogin() {
+    String text = _searchResult.text;
+
+    print('login attempt: $text');
+  }
+}
